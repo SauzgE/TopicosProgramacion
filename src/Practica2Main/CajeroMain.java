bfb377803f4f2dee299a40e1f4def5f0a58862eb
23031373 <23031373@itcelaya.edu.mx>
2026-02-17 14:24:00 -0600

    Segundocommit

---
+++ src/Practica2Main/CajeroMain.java
@@ -0,0 +1,9 @@
+package Practica2Main;
+
+import Practica2Controllers.CajeroController;
+
+public class CajeroMain {
+    public static void main(String[] args) {
+        new CajeroController().iniciar();
+    }
+}
